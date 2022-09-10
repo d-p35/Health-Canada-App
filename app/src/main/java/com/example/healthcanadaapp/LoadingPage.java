28f182d0349edb54f27231d610ef3b9cb6c65706
Vikram Narra <vchandra.narra@gmail.com>
2022-09-10 10:55:32 -0500

    LoadingPage
    CreatedALodingPage

---
+++ app/src/main/java/com/example/healthcanadaapp/LoadingPage.java
@@ -0,0 +1,37 @@
+package com.example.healthcanadaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class LoadingPage extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_page);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        progressBar = findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.loading_text_view);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        progressAnimation();
+    }
+
+    public void progressAnimation() {
+        ProgressBarAnimation anim = new ProgressBarAnimation(this, progressBar, textView, 0f, 100f);
+        anim.setDuration(8000);
+        progressBar.setAnimation(anim);
+    }
+
+}
